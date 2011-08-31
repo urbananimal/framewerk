01b08e177e62dc4bd640db8a21c08aab0eaab22a
John Smith <john@urbanplum.eu>
2011-08-31 15:59:44 +0000

    Implmented correct application error handling for JSON-RPC 2.0 - not yet handling JSON-RPC specific errors or Application exceptions. Coming soon.

--- Views/ViewJSONRPC.php
+++ Views/ViewJSONRPC.php
@@ -20,6 +20,47 @@ class Framewerk_Views_ViewJSONRPC extends Framewerk_Views_View
 			$rendered_elements[$element_name] = $this->renderElement($element_name, true);
 		}
 
+		// Has there been an error?
+		if( ($errors = $this->renderNotices(Framewerk_Notice::TYPE_ERROR)) )
+		{
+			$invalid_fields = array();
+
+			foreach($this->view_data as $name => $input_data_object)
+			{
+				if( !($input_data_object instanceof Framewerk_InputData) || $input_data_object->isValid() ) continue;
+				$invalid_fields[] = $name;
+			}
+
+			$resp_type = array
+			(
+				'error' => array
+				(
+					'code' => 1,
+					'message' => 'APPLICATION_ERROR',
+					'data' => array
+					(
+						'notices' => $errors,
+						'invalid_fields' => $invalid_fields
+					)
+				)
+			);
+		}
+		else
+		{
+			$resp_type = array
+			(
+				'result' => array
+				(
+					'view_data' => $this->view_data,
+					'elements' => $rendered_elements,
+					'template' => $this->template_name ? $this->captureTemplateOutput(Config::getTemplatePath().'/'.$this->controller.'/'.$this->template_name.'.tpl.php') : null,
+					'redirect' => $this->redirect_location,
+					'title' => $this->getTitle(),
+					'notices' => $this->renderNotices(Framewerk_Notice::TYPE_SUCCESS)
+				)
+			);
+		}
+
 		header('Cache-Control: no-cache, must-revalidate');
 		header('Expires: Fri, 20 Dec 1985 12:00:00 GMT');
 		header('Content-type: application/json-rpc');
@@ -28,15 +69,7 @@ class Framewerk_Views_ViewJSONRPC extends Framewerk_Views_View
 		(
 			'id' =>  $this->request_id,
 			'jsonrpc' => '2.0',
-			'result' => array
-			(
-				'view_data' => $this->view_data,
-				'elements' => $rendered_elements,
-				'template' => $this->template_name ? $this->captureTemplateOutput(Config::getTemplatePath().'/'.$this->controller.'/'.$this->template_name.'.tpl.php') : null,
-				'redirect' => $this->redirect_location,
-				'title' => $this->getTitle()
-			)
-		);
+		) + $resp_type;
 
 		echo json_encode($response);
 		exit;
@@ -89,7 +122,7 @@ class Framewerk_Views_ViewJSONRPC extends Framewerk_Views_View
 	 * 
 	 * @see Framewerk_View::renderNotices()
 	 */
-	protected function renderNotices($notice_type)
+	protected function renderNotices($notice_type = null)
 	{
 		return Framewerk_Notice::getNotices($notice_type);
 	}
